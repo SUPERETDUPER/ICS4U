2546e993258a2bc75fc488270a5a922978dcf6b3
SUPERETDUPER <machst@live.ca>
2018-02-06 13:40:18 -0500

    Basic working version

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,28 +2,41 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 
-public class Main extends Application implements EventHandler<MouseEvent> {
-
-    ImageView avatar;
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Salutation et Avatar");
 
-        avatar = new ImageView();
+        BorderPane layout = new BorderPane();
+
+
+        final Text bienvenue = new Text("Bonjour! Mon nom est Martin!");
+        bienvenue.setFont(Font.font(null, 20));
+        layout.setTop(bienvenue);
+        BorderPane.setAlignment(bienvenue, Pos.CENTER);
+
+        final Text popup = new Text("Surprise!");
+        popup.setFont(Font.font(null, 20));
+        popup.setVisible(false);
+        layout.setBottom(popup);
+        BorderPane.setAlignment(popup, Pos.CENTER);
+
+        final ImageView avatar = new ImageView();
 
         avatar.setImage(new Image(new FileInputStream("res/avatar.png")));
-
         avatar.setPreserveRatio(true);
 
         avatar.setOnMouseEntered(new EventHandler<MouseEvent>() {
@@ -43,26 +56,16 @@ public class Main extends Application implements EventHandler<MouseEvent> {
         avatar.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-
+                popup.setVisible(!popup.isVisible());
             }
         });
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(avatar);
+        layout.setCenter(avatar);
 
-        Scene scene = new Scene(layout, 600, 300);
+        Scene scene = new Scene(layout, 600, 600);
 
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    @Override
-    public void handle(MouseEvent event) {
-        switch (event.getEventType()){
-            case MouseEvent.MOUSE_ENTERED:
-                avatar.setFitHeight(300);
-
-        }
     }
 
     public static void main(String[] args) {
