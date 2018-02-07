25a147c3c984b6bb4f01d6ff4cf9832c814e7819
SUPERETDUPER <machst@live.ca>
2018-02-07 13:40:02 -0500

    Empty scene

--- src/main/Main.java
+++ src/main/Main.java
@@ -25,6 +25,8 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -35,6 +37,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setTitle("Div et Mod");
 
+        VBox layout = new VBox();
+
+
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
