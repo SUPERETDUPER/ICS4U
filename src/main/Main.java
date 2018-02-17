5e2fe855280fb104a0c3a54143f85628dbd3c080
SUPERETDUPER <machst@live.ca>
2018-02-17 17:51:38 -0500

    Renamed variables and added Title

--- src/main/Main.java
+++ src/main/Main.java
@@ -42,25 +42,30 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Main extends Application {
-    private static final String TITRE_DE_FENETRE = "Div et Mod";
+    private static final String TITRE = "Div et Mod";
 
-    private static final String SUGGESTION_POUR_FIELD = "Entrez un nombre ici";
+    private static final String SUGGESTION_BOITE_DE_TEXTE = "Entrez un nombre ici";
     private static final String DESCRIPTION_ENTREE_1 = "Premier numéro";
     private static final String DESCRIPTION_ENTREE_2 = "Deuxième numéro";
-    private static final String ERREUR_INPUT_INVALIDE = "'%s' n'est pas un numéro valide\n"; //New line pour que la hauteur ne change pas
-    private static final String TXT_PROMT = "Entrez les numéros\n"; //New line pour que la hauteur ne change pas
 
+    private static final String MSG_DIVISON_PAR_ZERO = "Impossible de diviser par zéro";
+    private static final String MSG_ENTREE_INVALIDE = "'%s' n'est pas un numéro valide\n"; //New line pour que la hauteur ne change pas
+    private static final String MSG_ENTREE_VIDE = "Entrez les numéros\n"; //New line pour que la hauteur ne change pas
+
+    private static final Font FONT_TITRE = Font.font(null, FontWeight.EXTRA_BOLD, 35);
     private static final Font FONT_NORMAL = Font.font(15);
     private static final Font FONT_BOLD = Font.font(null, FontWeight.BOLD, 15);
-    private static final int LINE_SPACING = 10;
+    private static final int INTERLINE = 10;
 
-    private static final int PADDING = 15;
-    private static final int SPACING_VBOX = 20;
-    private static final int ESPACE_H_TABLEAU = 20;
-    private static final int ESPACE_V_TABLEAU = 10;
     private static final int HAUTEUR_FENETRE = 300;
     private static final int LARGUEUR_FENETRE = 800;
-    private Text reponse;
+    private static final int PADDING_FENETRE = 15;
+
+    private static final int ESPACE_VBOX = 30;
+    private static final int ESPACE_H_TABLEAU = 20;
+    private static final int ESPACE_V_TABLEAU = 10;
+
+    private Text txtReponse;
 
     public static void main(String[] args) {
         launch(args);
@@ -68,17 +73,27 @@ public class Main extends Application {
 
     @Override
     public void start(@NotNull Stage primaryStage) {
-        primaryStage.setTitle(TITRE_DE_FENETRE);
+        primaryStage.setTitle(TITRE);
+
+        //Titre
+        Text txtTitre = new Text(TITRE);
+        txtTitre.setFont(FONT_TITRE);
 
         //Zone de text où la réponse est montrée
-        reponse = new Text(TXT_PROMT);
-        reponse.setLineSpacing(LINE_SPACING);
-        reponse.setFont(FONT_BOLD);
+        txtReponse = new Text(MSG_ENTREE_VIDE);
+        txtReponse.setLineSpacing(INTERLINE);
+        txtReponse.setFont(FONT_BOLD);
 
         //Création du layout principal
-        VBox layout = new VBox(creeZoneEntree(), new Separator(Orientation.HORIZONTAL), reponse);
-        layout.setSpacing(SPACING_VBOX);
-        layout.setPadding(new Insets(PADDING));
+        VBox layout = new VBox(
+                txtTitre,
+                creeZoneEntree(),
+                new Separator(Orientation.HORIZONTAL),
+                txtReponse
+        );
+
+        layout.setSpacing(ESPACE_VBOX);
+        layout.setPadding(new Insets(PADDING_FENETRE));
         layout.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layout, LARGUEUR_FENETRE, HAUTEUR_FENETRE);
@@ -93,13 +108,27 @@ public class Main extends Application {
         final TextField deuxiemeTxtField = creeBoiteEntreeDeTexte();
 
         //Ajouter les listeners
-        premierTxtField.textProperty().addListener((observable, oldValue, newValue) -> montrerReponse(newValue, deuxiemeTxtField.getText()));
-        deuxiemeTxtField.textProperty().addListener((observable, oldValue, newValue) -> montrerReponse(premierTxtField.getText(), newValue));
+        premierTxtField.textProperty().addListener(
+                (observable, oldValue, newValue) -> montrerReponse(newValue, deuxiemeTxtField.getText())
+        );
+
+        deuxiemeTxtField.textProperty().addListener(
+                (observable, oldValue, newValue) -> montrerReponse(premierTxtField.getText(), newValue)
+        );
 
         //Création du grid
         GridPane inputPane = new GridPane();
-        inputPane.addColumn(0, creeDescription(DESCRIPTION_ENTREE_1), creeDescription(DESCRIPTION_ENTREE_2));
-        inputPane.addColumn(1, premierTxtField, deuxiemeTxtField);
+
+        inputPane.addRow(0,
+                creeDescription(DESCRIPTION_ENTREE_1),
+                premierTxtField
+        );
+
+        inputPane.addRow(1,
+                creeDescription(DESCRIPTION_ENTREE_2),
+                deuxiemeTxtField
+        );
+
         inputPane.setVgap(ESPACE_V_TABLEAU);
         inputPane.setHgap(ESPACE_H_TABLEAU);
         inputPane.setAlignment(Pos.CENTER);
@@ -111,7 +140,7 @@ public class Main extends Application {
     private static TextField creeBoiteEntreeDeTexte() {
         TextField boiteInput = new TextField();
         boiteInput.setFont(FONT_NORMAL);
-        boiteInput.setPromptText(SUGGESTION_POUR_FIELD);
+        boiteInput.setPromptText(SUGGESTION_BOITE_DE_TEXTE);
         boiteInput.setAlignment(Pos.BASELINE_RIGHT);
         return boiteInput;
     }
@@ -125,7 +154,7 @@ public class Main extends Application {
 
     private void montrerReponse(@NotNull String entree1, @NotNull String entree2) {
         if (entree1.isEmpty() || entree2.isEmpty()) {
-            setReponseBold(TXT_PROMT);
+            setReponseBold(MSG_ENTREE_VIDE);
             return;
         }
 
@@ -147,26 +176,26 @@ public class Main extends Application {
         try {
             return Long.parseLong(entree);
         } catch (NumberFormatException e) {
-            setReponseBold(String.format(ERREUR_INPUT_INVALIDE, entree));
+            setReponseBold(String.format(MSG_ENTREE_INVALIDE, entree));
             return null;
         }
     }
 
     private void setReponseBold(@NotNull String text) {
-        reponse.setFont(FONT_BOLD);
-        reponse.setText(text);
+        txtReponse.setFont(FONT_BOLD);
+        txtReponse.setText(text);
     }
 
     private void setReponseNormal(@NotNull String text) {
-        reponse.setFont(FONT_NORMAL);
-        reponse.setText(text);
+        txtReponse.setFont(FONT_NORMAL);
+        txtReponse.setText(text);
     }
 
     @NotNull
     @Contract(pure = true)
     private static String creeReponse(long dividende, long diviseur) {
         if (diviseur == 0) {
-            return "Impossible de diviser par zéro";
+            return MSG_DIVISON_PAR_ZERO;
         }
 
         return dividende
